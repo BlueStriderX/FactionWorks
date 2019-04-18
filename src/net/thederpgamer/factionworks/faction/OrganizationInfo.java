6a4b51087b89e06e4a257bef40c2568ea6c03461
TheDerpGamer <garret2kool@gmail.com>
2019-04-18 11:19:24 -0700

    made base files

---
+++ src/net/thederpgamer/factionworks/faction/OrganizationInfo.java
@@ -0,0 +1,7 @@
+package net.thederpgamer.factionworks.faction;
+
+public class OrganizationInfo {
+	
+	public static String governmentType;
+	public static 
+}
---
+++ src/net/thederpgamer/factionworks/main/Main.java
@@ -0,0 +1,12 @@
+package net.thederpgamer.factionworks.main;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("FactionWorks is loading!");
+	}
+}
