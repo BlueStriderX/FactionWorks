a7da5020499af540adb57df6d7ebc17e7e4e9d1b
TheDerpGamer <garret2kool@gmail.com>
2019-04-18 13:34:39 -0700

    Update Organization.java

--- src/net/thederpgamer/factionworks/faction/Organization.java
+++ src/net/thederpgamer/factionworks/faction/Organization.java
@@ -9,10 +9,14 @@ import net.distortsm.api.player.Player;
 import net.distortsm.api.player.Faction;
 
 public class Organization {
+
+		public static ArrayList<Faction> memberFactions = new ArrayList<Faction>();
+		public static Multimap<Faction, Player> members = ArrayListMultimap.create();
+		public static ArrayList<Faction> enemyFactions = new ArrayList<Faction>();
+		public static OrganizationInfo organizationInfo = new OrganizationInfo();
+		public static UUID orgUUID = UUID.randomUUID();
 	
-	public static ArrayList<Faction> memberFactions = new ArrayList<Faction>();
-	public static Multimap<Faction, Player> members = ArrayListMultimap.create();
-	public static ArrayList<Faction> enemyFactions = new ArrayList<Faction>();
-	public static OrganizationInfo orgDescription = new OrganizationInfo();
-	public static UUID orgUUID = UUID.randomUUID();
+	public static OrganizationInfo getOrganizationInfo() {
+		return organizationInfo;
+	}
 }
