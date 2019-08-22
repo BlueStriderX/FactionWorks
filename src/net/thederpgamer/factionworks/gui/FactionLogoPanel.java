e790cdf738a751025f17274b774e9cab22429afa
TheDerpGamer <garret2kool@gmail.com>
2019-08-21 19:45:05 -0700

    Update FactionLogoPanel.java

--- src/net/thederpgamer/factionworks/gui/FactionLogoPanel.java
+++ src/net/thederpgamer/factionworks/gui/FactionLogoPanel.java
@@ -1,30 +1,58 @@
 package net.thederpgamer.factionworks.gui;
 
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+import org.schema.game.client.data.GameClientState;
+import org.schema.schine.graphicsengine.core.Controller;
+import org.schema.schine.graphicsengine.core.GlUtil;
 import org.schema.schine.graphicsengine.forms.gui.GUIElement;
 import org.schema.schine.input.InputState;
 
-public class FactionLogoPanel extends GUIElement {
+@SuppressWarnings("deprecation")
+public class FactionLogoPanel extends GUIElement implements Observer {
 
-	public FactionLogoPanel(InputState var1) {
+	public float width;
+	public float height;
+	public boolean updateNeeded;
+	public String factionLogoPath = "..\\";
+	
+	public FactionLogoPanel(InputState var1, float inputWidth, float inputHeight) {
 		super(var1);
+		this.width = inputWidth;
+		this.height = inputHeight;
+		((GameClientState) this.getState()).getFactionManager().addObserver(this);
 	}
 
 	@Override
 	public void cleanUp() {
-		// TODO Auto-generated method stub
-		
+		((GameClientState) this.getState()).getFactionManager().deleteObserver(this);		
 	}
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		if (this.needsReOrientation()) {
+			this.doOrientation();
+		}
+
+		GlUtil.glPushMatrix();
+		this.transform();
+		this.draw();
+		GlUtil.glPopMatrix();
 	}
 
 	@Override
 	public void onInit() {
-		// TODO Auto-generated method stub
+		try {
+			Controller.getResLoader().getImageLoader().loadImage(factionLogoPath,"faction_logo.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+	}
+
+	public void update(Observable var1, Object var2) {
+		this.updateNeeded = true;
 	}
 
 	@Override
@@ -38,5 +66,5 @@ public class FactionLogoPanel extends GUIElement {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
 }
