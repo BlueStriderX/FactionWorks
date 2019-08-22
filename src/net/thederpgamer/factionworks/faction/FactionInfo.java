f61827957c6f5cf6166004546caeab0eb596f324
TheDerpGamer <garret2kool@gmail.com>
2019-08-21 17:20:41 -0700

    Update FactionInfo.java

--- src/net/thederpgamer/factionworks/faction/FactionInfo.java
+++ src/net/thederpgamer/factionworks/faction/FactionInfo.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.schema.game.common.data.player.faction.Faction;
 import org.schema.game.common.data.player.faction.FactionRelation.RType;
 import org.schema.schine.network.StateInterface;
-
 public class FactionInfo extends Faction {
 	
 	public FactionInfo(StateInterface var1) {
@@ -14,14 +13,22 @@ public class FactionInfo extends Faction {
 	
 	public Organization factionOrg;
 	public RType relation;
-	public String leaderName;
+	public long leader;
 	public PlayerRank rank;
 	public String shipTag;
 	public Government government;
-	public String details;
+	public String factionDetails;
 	public File logo;
 	
 	public Government getGovernment() {
 		return this.government;
+	}
+	
+	public Organization getOrganization() {
+		return this.factionOrg;
+	}
+	
+	public String getFactionDetails() {
+		return this.factionDetails;
 	}
 }
